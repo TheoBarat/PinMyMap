55c04a5b52a85a3fe3e2af9c97c0c82268374155
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2024-10-18 09:46:12 +0200

    [ADD] Ajout de la map + navbar
    Mise en place de la navbar et de la map avec leaflet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,59 @@
 <template>
   <div id="app">
-    <UserList />  <!-- Utilise ton composant ici -->
+    <nav>
+      <img alt="Logo" src="./assets/PinMyMapLogo.png" style="height: 80px; width: 80px;"/>
+      <ul class=navbar>
+        <li><a href="#" style="color: white; text-decoration: none;">Home</a></li>
+        <li><a href="#" style="color: white; text-decoration: none;">About</a></li>
+        <li><a href="#" style="color: white; text-decoration: none;">Contact</a></li>
+      </ul>
+    </nav>
+    <div id="map" style="height: 90vh; width: 100vw; position: absolute; top: 10vh; right: 0;"></div>
   </div>
 </template>
 
 <script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import UserList from './components/UserList.vue';  // Importer le composant
 
 export default {
-  name: 'App',
-  components: {
-    UserList,  // Enregistrer le composant
-  },
+
+  mounted() {
+    // Initialiser la carte
+    const map = L.map('map').setView([43.60221958291301, 1.4557566643659308], 4);
+
+    // Ajouter une couche de tuiles (OpenStreetMap)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 };
 </script>
 
 <style>
+  nav {
+    /* Styles pour la barre de navigation */
+    height: 10vh; 
+    width: 100vw; 
+    display: flex; 
+    align-items: center; 
+    justify-content: space-between; 
+    background-color: #333; 
+    color: white; 
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    right: 0; 
+    margin: 0; 
+    padding: 0;
+  }
+  .navbar {
+    /* Styles pour la liste de navigation */
+    display: flex; 
+    list-style-type: none; 
+    margin: 0;
+    padding: 10;
+  }
 /* Styles globaux pour ton application */
 </style>
